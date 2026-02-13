51d16bdc1b459a8a8e0a36c627d440961be6a551
pipe <pipe@felipeoidor.com>
2026-02-12 21:47:50 -0500

    feat(flask): render invoice form template

--- my_app/views/invoice/invoice_form.py
+++ my_app/views/invoice/invoice_form.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("invoice_form", __name__, url_prefix="/crear-factura")
 
 
 @bp.route("/")
 def hello_world():
-    return "<h1>Crear Factura</h1>"
+    return render_template("invoice_form/base-form.html")
